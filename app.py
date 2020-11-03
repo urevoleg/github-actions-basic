092b1569c0cac8c58c6a70009b271f5e690cd7c8
Oleg <urevolegg@gmail.com>
2020-11-03 23:17:03 +0300

    init commit, simple app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return """<div style="border:solid green 2px; padding: 20px"> <b>It's the best apps</b><br>👍</div>"""
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', threaded=True)
