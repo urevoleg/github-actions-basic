da2a593801d7d8d8adf296fca7dc22bc08e96779
Oleg <urevolegg@gmail.com>
2020-11-05 17:34:28 +0300

    add port options

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello_world():
     return """<div style="border:solid green 2px; padding: 20px"> <b>It's the best apps</b><br>👍</div>"""
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=9876, threaded=True)
