e77ac893d3561e0fbeb9dfabb124009d05c1ecf1
Oleg <urevolegg@gmail.com>
2020-11-05 18:27:25 +0300

    fix text in div

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return """<div style="border:solid green 2px; padding: 20px"> <b>It's the best apps</b><br>👍</div>"""
+    return """<div style="border:solid green 2px; padding: 20px"> <b>It's the best apps from github-actions</b><br>👍</div>"""
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=9876, threaded=True)
